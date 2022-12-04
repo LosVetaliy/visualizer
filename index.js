a77f765e6691141853540404807011397a85481f
Dmitry Yakovlev <losvetaliy@gmail.com>
2022-12-04 18:35:15 +0300

    flame style work

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import { Track } from "./track.js";
+import { flame } from "./visualizers/flame.js";
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const visualizers = [
+    flame
+]
 
 const run = () => {
     const track = new Track('./static/GORYACHOHOLODNO_175_135.mp3') 
+    const renderFrame = () => {
+        requestAnimationFrame(renderFrame);
+
+        const width = window.innerWidth || 0
+        const height = window.innerHeight || 0
+
+        canvas.width = width;
+        canvas.height = height;
+        ctx.clearRect(0, 0, width, height)
+        // for (const visualization of visualizers) {
+        //   visualization.apply({ctx, width, height, centerX: width / 2, centerY: height / 2, freqs: track.getByteFrequencyData()});
+        // }
+        for (const visualization of visualizers) {
+            visualization.apply({ctx, width, height, freqs: track.getByteFrequencyData()});
+        }
+    };
+    renderFrame()
 }
 run()
--- track.js
+++ track.js
@@ -6,5 +6,19 @@ export class Track {
         this.audio.loop = true;
 
         this.audio.play();
+
+        this.context = new AudioContext();
+        this.analyser = this.context.createAnalyser();
+        this.source = this.context.createMediaElementSource(this.audio);
+        this.source.connect(this.analyser);
+        this.source.connect(this.context.destination);
+
+        this.analyser.fftSize = 1024;
+    }
+    getByteFrequencyData() {
+        const data = new Uint8Array(this.analyser.frequencyBinCount);
+        this.analyser.getByteFrequencyData(data);
+    
+        return data;
     }
 }
